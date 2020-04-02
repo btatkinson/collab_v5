c1386e52c430ceca6a975be6f81f04a849cd385e
Blake Atkinson <blaketatkinson@gmail.com>
2020-04-02 11:13:21 -0400

    front input panel done

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <?php get_header(); ?>
 </head>
 <body>
+    <!-- top image panel -->
     <div class="splash" style="background-image: url(<?php echo get_theme_mod('home_image'); ?>);">
         <div class="splash-panel">
             <div class="sp-body">
@@ -24,6 +25,67 @@
         <br>
         <br>
         <a class="sp-button" href="<?php echo get_permalink(get_page_by_title('Get Started')) ?>" role="button">Get Started <i class="fas fa-angle-right sp-arrow"></i></a>
+    </div>
+    <!-- middle input panel -->
+    <div class="middle-input-panel">
+        <div class="mip-left">
+        <h1 class="slogan">
+            <?php //echo get_theme_mod('_themename_intro_text'); ?>
+            Ending your marriage doesn’t have to be destructive. <span style="font-family: 'Pacifico', cursive;" class="slogan-emphasis">Explore options. Create solutions.</span>
+        </h1>
+        <h3 class="slogan-sub">Find A Collaborative Professional Today:</h3>
+        <form class="fp-form">
+            <div class="form-row">
+                <div class="form-group form-width">
+                    <label for="firstName" class="form-control">First Name</label>
+                    <input
+                    type="text"
+                    class="form-control"
+                    id="fnameInput"
+                    placeholder="First Name"
+                    aria-label="First name text input"
+                    >
+                </div>
+                <div class="form-group form-width">
+                    <label for="lastName" class="form-control">Last Name</label>
+                    <input
+                    type="text"
+                    class="form-control"
+                    id="lnameInput"
+                    placeholder="Last Name"
+                    aria-label="Last name text input"
+                    >
+                </div>
+                <div class="form-group form-width">
+                    <label for="occSelect" class="form-control">Profession</label>
+                    <select id="occSelect" class="custom-select">
+                    <option selected>Type (Any)</option>
+                    <option value="Attorney">Attorney</option>
+                    <option value="Financial Specialist">Financial Specialist</option>
+                    <option value="Family Relations">Family Relations</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+        <div class="mip-btn-row">
+            <button
+            class="btn btn-secondary mip-btn"
+            id="findProBrowse"
+            onclick="window.location.href=('<?php echo get_permalink(get_page_by_title('Find A Professional')) ?>')"
+            >
+            Browse <i class="fas fa-search"></i></button>
+            <button
+            class="btn btn-primary mip-btn"
+            id="findProSub"
+            onclick="window.location.href=('<?php echo get_permalink(get_page_by_title('Find A Professional')) ?>')"
+            type="submit" >
+            Search <i class="fas fa-arrow-right"></i></button>
+        </div>
+        </div>
+        <div class="col-lg-4 fp-portrait-col">
+        <div class="fp-box d-flex align-items-bottom" style="background-image: url(<?php echo get_theme_mod('_themename_banner_image'); ?>);">
+        </div>
+        </div>
     </div>
 </body>
 <?php get_footer(); ?>
