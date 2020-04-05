00b715f028de4d2db6dadbbd26fc949817a380e8
Blake Atkinson <blaketatkinson@gmail.com>
2020-04-05 14:57:40 -0400

    finished get started

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -15,6 +15,7 @@ function enqueue_js() {
     wp_deregister_script( 'jquery' );
 	wp_register_script( 'jquery', get_template_directory_uri() . '/assets/js/jquery.js' );
     wp_enqueue_script( 'home', get_template_directory_uri() . '/assets/js/home.js', array( 'jquery' ) );
+    wp_enqueue_script( 'getstarted', get_template_directory_uri() . '/assets/js/getstarted.js', array( 'jquery' ) );
   
 }
   
