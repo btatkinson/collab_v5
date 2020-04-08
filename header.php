bc8f4ce92f5fb63d97fe4bc78e5afc6edaf00fc4
Blake Atkinson <blaketatkinson@gmail.com>
2020-04-08 08:13:28 -0400

    fixed mobile

--- header.php
+++ header.php
@@ -1,15 +1,16 @@
 
 <!-- load scripts -->
-<meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="initial-scale=1.0, width=device-width">
-<?php wp_head() ?>
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-<!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Lora|LibreFranklin:100,100&display=swap" rel="stylesheet">
-<!-- Fontawesome -->
-<script src="https://kit.fontawesome.com/523fd0d64e.js" crossorigin="anonymous"></script>
-<title>Collaborative Law</title>
-
+<head>
+    <?php wp_head() ?>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora|LibreFranklin:100,100&display=swap" rel="stylesheet">
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/523fd0d64e.js" crossorigin="anonymous"></script>
+    <title>Collaborative Law</title>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
 <!-- front page nav bar -->
 <?php if(is_front_page()) { ?>
     <div class="vanity-bar">
